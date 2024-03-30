9d479175c454d2f3cef678dbbe6627d46d6a0fda
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2024-03-30 09:48:36 +0900

    test

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js"
-pin "./likes", to: "likes.js"
+pin "application", to: "application.js", preload: true
+pin "likes", to: "likes.js"
